8dbd359610c2b85292adc186178593ea9d4e94c3
Lugen-dl <gena90213@gmail.com>
2026-04-14 21:18:49 +0300

    feat: other storage for secrets

--- terraform/sensitive-var.tf
+++ terraform/sensitive-var.tf
@@ -1,3 +1,4 @@
+#---Digitalocean secret---
 variable "do_token" {
   sensitive = true
   default = true
@@ -12,8 +13,13 @@ variable "s3_access" {
   type = string
   default = true
 }
-
+#---Cloudflare secret---
 variable "cloudflare_api" {
+  sensitive = true
+  default = true
+}
+
+variable "cloudflare_id" {
   sensitive = true
   default = true
 }
