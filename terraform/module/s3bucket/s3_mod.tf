dd6e330a5c55450d7b903b048587d221322e6eec
Lugen-dl <gena90213@gmail.com>
2026-04-14 23:39:02 +0300

    refactor

--- terraform/module/s3bucket/s3_mod.tf
+++ terraform/module/s3bucket/s3_mod.tf
@@ -9,7 +9,7 @@ terraform {
 
 #Creating our bucket
 resource "digitalocean_spaces_bucket" "bucket_id" {
-  name = "unique-s3-bucket-iac-isw8"
+  name = "unique-s3-bucket-iac-isw9"
   region = var.region
   acl = "public-read"
 }
