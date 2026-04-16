7441489e26750f7e31eebf3d558917e39a403495
Lugen-dl <gena90213@gmail.com>
2026-04-16 22:21:09 +0300

    .

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,7 +13,19 @@ terraform {
       version = "~> 4.0"
     }
   }
-  backend "s3" {}
+
+  backend "s3" {
+    bucket = "tf-state"
+    key = "keys/terraform.tfstate"
+    region = "us-east-1"
+
+    endpoint =  "https://ams3.digitalocean.com"
+
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    skip_region_validation = true
+    force_path_style = true
+  }
 }
 
 provider "digitalocean" {
