5a2b0aac46c9fe960570e346f44bd53ff6764fcc
Lugen-dl <gena90213@gmail.com>
2026-04-17 23:53:33 +0300

    fix: changing policy request

--- terraform/module/firewall/firewall_mod.tf
+++ terraform/module/firewall/firewall_mod.tf
@@ -17,28 +17,29 @@ resource "digitalocean_firewall" "firewall_id" {
 #    source_addresses = [ "${var.my_public_ip}/32"]
 #  }
 #
+
     inbound_rule {
     protocol = "tcp"
     port_range = "22"
-    source_addresses = [ "0.0.0.0/0", "::/0" ]
+    source_addresses = [ "0.0.0.0/0" ] #Only our server in DO can enter with ssh
   }
 
   inbound_rule {
     protocol = "tcp"
     port_range = "80"
-    source_load_balancer_uids = [ var.ld_id ]
+    source_load_balancer_uids = [ var.ld_id ] #filtrating requests ONLY with lb
   }
 
   outbound_rule {
     protocol = "tcp"
-    port_range = "1-65535"
+    port_range = "all" #"all" cause, it will be easier to update our server conf packages
     destination_addresses = [ "0.0.0.0/0", "::/0" ]
   }
 
 
   outbound_rule {
     protocol              = "udp"
-    port_range            = "1-65535"
+    port_range            = "1-65535" #keeping it so, our server won't have any problems with dns requests
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
